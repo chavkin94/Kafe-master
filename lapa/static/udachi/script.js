3850246ba50142d5f005b795b051fba1d6e3e297
Andrysha <chavkin94@mail.ru>
2021-04-29 23:20:37 +0900

    пара 5

--- lapa/static/udachi/script.js
+++ lapa/static/udachi/script.js
@@ -13,7 +13,7 @@ function dobavlenie_v_korzinu(tovar_id, kol_vo) {
     }
 
     let naidenii_tovar = tovari.find(tovar => tovar.id_tovara === tovar_id);
-    console.log(naidenii_tovar);
+    // console.log(naidenii_tovar);
     if (naidenii_tovar === undefined) {
         tovari.push({
             'id_tovara': tovar_id,
@@ -21,23 +21,22 @@ function dobavlenie_v_korzinu(tovar_id, kol_vo) {
         })
     }
 
-    console.log(tovari);
+    // console.log(tovari);
 
     localStorage.setItem('tovari', JSON.stringify(tovari))
 
-    $('.alert_bluda').show();
-}
-
-
-$(function () {
-    $('.alert_bluda').hide();
-
+    $('.alert_tovar').show();
+    $(function () {
+    $('.alert_tovar').hide();
     $('.btn_dobavlenie_v_korzinu').click(function (e) {
         e.preventDefault();
-        let tovar_id = e.currentTarget.dataset['idBluda'];
+        let tovar_id = e.currentTarget.dataset['id_tovara'];
         dobavlenie_v_korzinu(tovar_id, 1);
         $(e.currentTarget).text('Товар в корзине');
         $(e.currentTarget).toggleClass('btn-outline-success');
         $(e.currentTarget).toggleClass('btn-success');
     })
 })
+}
+
+
